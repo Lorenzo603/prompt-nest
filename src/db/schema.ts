1f02870a0cb460bb64da7a1b4778763d7208d312
Lorenzo Furrer <lorenzo.furrer@pearson.com>
2025-06-10 17:12:59 +0100

    convert creation date to timestamp

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
-import { date, integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { date, integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const promptsTable = pgTable("prompts", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   text: text().notNull(),
-  creationDate: date().notNull(),
+  creationDate: timestamp().notNull(),
 });
