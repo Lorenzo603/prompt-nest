1cc393ca4892b5111beca035ebccb48e2c1babc2
Lorenzo Furrer <lorenzo.furrer@pearson.com>
2025-07-06 16:07:26 +0100

    fix checkpoint hit image zoom

--- src/components/CheckpointHit.js
+++ src/components/CheckpointHit.js
@@ -190,7 +190,6 @@ const CheckpointHit = ({ hit, onCheckpointUpdated }) => {
                       e.target.style.display = 'none';
                     }}
                   />
-                  <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-300"></div>
                 </div>
               </div>
             )}
